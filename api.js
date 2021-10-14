748c85fd6a0171e150da337e0e7b7e953422a6e4
Konan Houphoue <bahobab@hotmail.com>
2021-10-14 17:49:14 -0500

    clean up unecessary comment and update readme

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 const hubspot = require('@hubspot/api-client');
 
 const hubspotClient = new hubspot.Client({apiKey: process.env.API_KEY});
-// const hubspotClient = new hubspot.Client({apiKey: 'f938f2c1-d0f7-4dec-9d98-4096739ebf8d'});
 
 module.exports = {hubspotClient};
